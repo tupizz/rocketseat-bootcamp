4e7812d6613ee3b94c27799b6b5cb6427759c1f4
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-06-29 21:26:46 -0300

    Upload de arquivos, avatar file para User

--- node-typescript/src/routes/users.routes.ts
+++ node-typescript/src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentRouter = Router();
+const upload = multer(uploadConfig);
 
 appointmentRouter.post('/', async (request, response) => {
   try {
@@ -17,5 +22,27 @@ appointmentRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+appointmentRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const avatarService = new UpdateUserAvatarService();
+
+      const user = await avatarService.execute({
+        userId: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
 
 export default appointmentRouter;
--- node-typescript/src/server.ts
+++ node-typescript/src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import './database';
 
+import './database';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
