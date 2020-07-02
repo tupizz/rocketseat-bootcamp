e4f86f9cc329085a67dbf64736e5c0b7cf14cb9c
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-07-02 14:04:07 -0300

    front-end: hook para useAuth, signOut

--- gobarber-front-react-typescript/src/context/AuthContextProvider.tsx
+++ gobarber-front-react-typescript/src/context/AuthContextProvider.tsx
@@ -1,25 +1,67 @@
-import React, { createContext, useCallback } from 'react';
-import api from '../services/api';
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api, { SessionsPostModel } from '../services/api';
 
 interface AuthContextState {
-  name: string;
+  user: object;
   signIn(credentials: { email: string; password: string }): Promise<void>;
+  signOut(): void;
+}
+
+interface AuthUserState {
+  token: string;
+  user: object;
 }
 
 const AuthContext = createContext<AuthContextState>({} as AuthContextState);
 
 const AuthContextProvider: React.FC = ({ children }) => {
-  const signIn = useCallback(async ({ email, password }) => {
-    const response = await api.post('sessions', { email, password });
+  const [data, setData] = useState<AuthUserState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
 
-    console.log(response);
+    if (user && token) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthUserState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post<SessionsPostModel>('sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthUserState);
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Tadeu', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthContextProvider };
+const useAuth = (): AuthContextState => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return context;
+};
+
+export { AuthContextProvider, useAuth };
--- gobarber-front-react-typescript/src/services/api.ts
+++ gobarber-front-react-typescript/src/services/api.ts
@@ -2,4 +2,16 @@ import axios from 'axios';
 
 const api = axios.create({ baseURL: 'http://localhost:3333' });
 
+export interface SessionsPostModel {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    avatar: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  token: string;
+}
+
 export default api;
