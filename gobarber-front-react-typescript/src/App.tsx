6ba20de99905cd82eb8077c43c29dc67ce6e2985
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-07-02 11:14:46 -0300

    front-end: pagina sign in // insominia backup

--- gobarber-front-react-typescript/src/App.tsx
+++ gobarber-front-react-typescript/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/globals';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <SignUp />
+    <SignIn />
   </>
 );
 
--- gobarber-front-react-typescript/src/pages/SignIn/index.tsx
+++ gobarber-front-react-typescript/src/pages/SignIn/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (errors) {
+      formRef.current?.setErrors(getValidationErrors(errors));
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Go Barber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -27,7 +50,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="forgot">
           <FiLogIn />
